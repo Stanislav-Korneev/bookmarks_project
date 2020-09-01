864ea49b420d3debbe3e535c1cac8842d475088a
Stanislav <korstasvik@gmail.com>
2020-09-01 14:28:07 +0700

    добавил стрелку направления сортировки

--- src/components/pages/index/sortingPanel.vue
+++ src/components/pages/index/sortingPanel.vue
@@ -1,24 +1,27 @@
 <template>
-<div class="sorting-panel">
+<div
+  class="sorting-panel"
+  :class="{'sorting-panel_sorted' : sortMode.name, 'sorting-panel_reverse' : sortMode.reverse}"
+>
   Sort by:
   <button
     @click="sortBookmarks('sortBookmarksByName')"
     class="sorting-panel__button"
-    :class="{'sorting-panel__button_selected': sortMode === 'sortBookmarksByName'}"
+    :class="{'sorting-panel__button_selected': sortMode.name === 'sortBookmarksByName'}"
   >
     Name
   </button>
   <button
     @click="sortBookmarks('sortBookmarksByDate')"
     class="sorting-panel__button"
-    :class="{'sorting-panel__button_selected': sortMode === 'sortBookmarksByDate'}"
+    :class="{'sorting-panel__button_selected': sortMode.name === 'sortBookmarksByDate'}"
   >
     Date
   </button>
   <button
     @click="sortBookmarks('sortBookmarksByActivity')"
     class="sorting-panel__button"
-    :class="{'sorting-panel__button_selected': sortMode === 'sortBookmarksByActivity'}"
+    :class="{'sorting-panel__button_selected': sortMode.name === 'sortBookmarksByActivity'}"
   >
     Activity
   </button>
@@ -31,7 +34,7 @@ import store from '@/store/index';
 export default {
   computed: {
     sortMode() {
-      return store.state.bookmarksSorted.name;
+      return store.state.bookmarksSorted;
     },
   },
 
@@ -48,20 +51,31 @@ export default {
   display: flex;
   justify-content: space-between;
   align-items: center;
-  padding: .5rem 2rem;
-  width: 35rem;
+  padding: .5rem 2rem .5rem 3.5rem;
+  width: 36rem;
   height: 5rem;
   border-radius: 2rem;
   border: .2rem solid $blue;
   font-size: 1.5rem;
   font-weight: bold;
   color: $darkBlue;
+  transition: .3s;
+
+  &_sorted {
+    background-position: left 1rem center;
+    background-image: url("../../../assets/ico/arrow-down.svg");
+    background-repeat: no-repeat;
+  }
+
+  &_reverse {
+    background-image: url("../../../assets/ico/arrow-up.svg");
+  }
 
   @include mobile {
     margin-bottom: 1rem;
-    padding: .5rem 1rem;
+    padding: .5rem 1rem .5rem 3.5rem;
     width: 100%;
-    max-width: 35rem;
+    max-width: 36rem;
     font-size: 1.2rem;
   }
 
@@ -80,7 +94,7 @@ export default {
     }
 
     @include mobile {
-      width: 6.5rem;
+      width: 5.5rem;
     }
 
     &:focus {
